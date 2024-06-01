c446f7bc614a6c3b34f61bc37b424e520e8c940f
andreecamilo10 <andreecamilo10@gmail.com>
2024-06-01 16:51:39 +0000

    modificaciones en archivo arreglos_2

--- src/js/arreglos_2.js
+++ src/js/arreglos_2.js
@@ -13,4 +13,21 @@ juegos.forEach((elemento, indice, arr) => {
     console.log({elemento, indice, arr});
 });
 
-juegos.push('F-Zero');
+let nuevaLongitud = juegos.push('F-ZERO');
+console.log('Nueva Longitud: ' + {nuevaLongitud});
+
+nuevaLongitud = juegos.unshift('Fire-Emblem');
+console.log({nuevaLongitud, juegos});
+
+//.pop nos sirve para eliminar un elemento del arreglo
+let juegoBorrado = juegos.pop();
+console.log({juegoBorrado, juegos});
+
+let pos = 1;
+juegos.splice(pos, 2); //splice method
+
+let juegosBorrados = juegos.splice(pos, 2);
+console.log({juegosBorrados, juegos});
+
+let metroidIndex = juegos.indexOf('Metroid'); // caseSensitive
+console.log({metroidIndex});
